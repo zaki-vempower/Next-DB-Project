22c9e1f1ad7ad972a16a982ac71e32e1e122f684
zaki123007 <syed.zakiuddin@scalecapacity.com>
2025-03-10 04:03:50 +0530

    first api working

--- app/api/loads/route.ts
+++ app/api/loads/route.ts
@@ -15,10 +15,50 @@ export async function GET(_request: NextRequest, response: NextResponse) {
 
   try {
     const loads = await prisma.loads.findMany({
-        take: 100
+      take: 100,
+      select: {
+        id: true,
+        dispatched_city: true,
+        dispatched_state: true,
+        created_at: true,
+        pieces: true,
+        spot_load: true,
+        customer_id: true,
+        carrier_id: true,
+        equipment_type_id: true,
+        sales_rep_id: true,
+        carrier_rep_id: true,
+        ref_num: true,
+        status: true,
+        invoice_total: true,
+        carrier_total_rate: true,
+        origin_city: true,
+        origin_state: true,
+        pickup_date: true,
+        pickup_hours: true,
+        destination_city: true,
+        destination_state: true,
+        delivery_date: true,
+        delivery_hours: true,
+        pickup_count: true,
+        delivery_count: true,
+        miles: true,
+        pro: true,
+        description: true,
+      }
     });
 
-    return  new Response(loads, { status: 200, headers: { 'Content-Type': 'application/json' } }).json();
+    const responseBody = new ReadableStream({
+      start(controller) {
+        controller.enqueue(JSON.stringify(loads));
+        controller.close();
+      }
+    });
+    
+    return new Response(responseBody, {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' }
+    });
   } catch (error) {
     console.error(error)
     // @ts-expect-error Description: Prisma client type definitions are missing for the 'loads' table.
